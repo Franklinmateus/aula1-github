35ba8bcd01a5b0225ff95dcfce942783feebec78
Franklin Mateus <frankmateus660@gmail.com>
2020-06-29 18:46:29 -0300

    Acrescentada mensagem de bom dia

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -3,8 +3,10 @@ package aplication;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("Bom dia!");
+		
+		
 	}
 
 }
